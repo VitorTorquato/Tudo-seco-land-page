90fd3c4a6b428419d892fdf33c61c80650160f0c
Vitor <torquatovitor68@gmail.com>
2024-04-25 08:27:40 +0200

    building

---
+++ src/buttonMenu.js
@@ -0,0 +1,28 @@
+//hamburguer menu
+
+let menu = false;
+
+const btn = document.getElementById('toggle-menu');
+
+
+btn.addEventListener('click' , (e) => {
+
+       if(!menu){
+        document.querySelector('.fa-bars').classList.add('is-active');
+        document.querySelector('.mobile-nav').classList.add('is-active');
+        document.querySelector('.fa-x').classList.remove('is-active');
+       }else{
+        document.querySelector('.fa-bars').classList.remove('is-active');
+        document.querySelector('.mobile-nav').classList.remove('is-active');
+        document.querySelector('.fa-x').classList.add('is-active');
+       }
+
+
+       menu = !menu
+
+
+         
+
+});
+
+
--- src/main.js
+++ src/main.js
@@ -1,24 +1,3 @@
-let menu = false;
 
-const btn = document.getElementById('toggle-menu');
-
-
-btn.addEventListener('click' , (e) => {
-
-       if(!menu){
-        document.querySelector('.fa-bars').classList.add('is-active');
-        document.querySelector('.mobile-nav').classList.add('is-active');
-        document.querySelector('.fa-x').classList.remove('is-active');
-       }else{
-        document.querySelector('.fa-bars').classList.remove('is-active');
-        document.querySelector('.mobile-nav').classList.remove('is-active');
-        document.querySelector('.fa-x').classList.add('is-active');
-       }
-
-
-       menu = !menu
-
-
-         
-
-})
+import './buttonMenu.js';
+import './slide.js'
